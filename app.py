2f2b78d6145159cd8dd0265780de07927798ef97
Riddhika Arora <23ucc038@lnmiit.ac.in>
2025-05-01 01:43:38 +0530

    Changed Ticker to RELIANCE.NS

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 st.set_page_config(layout="wide")
 st.title("📈 Stock Price Prediction Dashboard")
 
-ticker = st.text_input("Enter Stock Ticker (e.g., RELIANCE.NS):", value="AAPL")
+ticker = st.text_input("Enter Stock Ticker (e.g., RELIANCE.NS):", value="RELIANCE.NS")
 
 if ticker:
    
