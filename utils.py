fe7122111bdd58c08ed3a60702029651232ad421
Riddhika Arora <23ucc038@lnmiit.ac.in>
2025-05-01 10:11:40 +0530

    Uncommented the fall back dataset

--- utils.py
+++ utils.py
@@ -49,17 +49,17 @@ def fetch_stock_data(ticker):
         except Exception as e:
             time.sleep(3)
 
-    # Fallback to saved CSV after retries fail
-    # try:
-    #     fallback_df = pd.read_csv("DS_Dataset_FinanceTrends.csv")
-    #     fallback_df["Date"] = pd.to_datetime(fallback_df["Date"],errors="coerce")
-    #     numeric_cols = ["Open", "High", "Low", "Close", "Volume"]
-    #     fallback_df[numeric_cols] = fallback_df[numeric_cols].apply(pd.to_numeric, errors="coerce")
-    #     fallback_df.dropna(inplace=True)  # ✅ Drop invalid rows
-    #     st.warning("Live data fetch failed. Using fallback data from DS_Dataset_FinanceTrends.csv.")
-    #     return fallback_df
-    # except FileNotFoundError:
-    #     raise ValueError("Both live fetch and fallback CSV failed. Please ensure reliance.csv is available.")
+  
+    try:
+        fallback_df = pd.read_csv("DS_Dataset_FinanceTrends.csv")
+        fallback_df["Date"] = pd.to_datetime(fallback_df["Date"],errors="coerce")
+        numeric_cols = ["Open", "High", "Low", "Close", "Volume"]
+        fallback_df[numeric_cols] = fallback_df[numeric_cols].apply(pd.to_numeric, errors="coerce")
+        fallback_df.dropna(inplace=True)  # ✅ Drop invalid rows
+        st.warning("Live data fetch failed. Using fallback data from DS_Dataset_FinanceTrends.csv.")
+        return fallback_df
+    except FileNotFoundError:
+        raise ValueError("Both live fetch and fallback CSV failed. Please ensure reliance.csv is available.")
 
 def add_features(df):
     """
